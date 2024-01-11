0a775ebfb188c5dc219364f85312675b9557b17e
rj-annamaria <annamaria.agocsova@gmail.com>
2024-01-11 20:45:59 +0100

    Changed HTML and created JS

---
+++ javascript/index.js
@@ -0,0 +1,52 @@
+function updateTime() {
+  let sanFranciscoElement = document.querySelector("#san-francisco");
+  if (sanFranciscoElement) {
+    let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+    let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+    let sanFranciscoTime = moment().tz("America/San_Francisco");
+
+    sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM	Do YYYY");
+    sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let melbourneElement = document.querySelector("#melbourne");
+  if (melbourneElement) {
+    let melbourneDateElement = melbourneElement.querySelector(".date");
+    let melbourneTimeElement = melbourneElement.querySelector(".time");
+    let melbourneTime = moment().tz("Australia/Melbourne");
+
+    melbourneDateElement.innerHTML = melbourneTime.format("MMMM	Do YYYY");
+    melbourneTimeElement.innerHTML = melbourneTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
